54b8d631712aaf55b3d43025e6b9916c92a5c494
Neevs1 <neevan.redkar@gmail.com>
2025-01-14 16:32:51 +0530

    Updated according to requirements

--- Assignment1/Assignment1.cpp
+++ Assignment1/Assignment1.cpp
@@ -6,9 +6,8 @@
 
 
 #include <GL/glut.h>
-#include <GL/gl.h>
-#include <GL/glu.h>
 #include <math.h>
+#define pi 3.14159
 
 void myInit(void){
   glClearColor(1.0,1.0,1.0,1.0);
@@ -21,21 +20,44 @@ void myInit(void){
 
 void display(void){
   glClear(GL_COLOR_BUFFER_BIT);
-  glBegin(GL_POINTS);
-   glVertex2i(100,50);
-   glVertex2i(100,130);
-   glVertex2i(150,130);
+  glBegin(GL_LINES);
+   glVertex2i(200,360);
+   glVertex2i(400,360);
+   glVertex2i(400,360);
+   glVertex2i(450,300);
+   glVertex2i(450,300);
+   glVertex2i(500,300);
+   glVertex2i(500,300);
+   glVertex2i(500,225);
+   glVertex2i(200,360);
+   glVertex2i(125,300);
+   glVertex2i(125,300);
+   glVertex2i(25,295);
+   glVertex2i(25,295);
+   glVertex2i(27,225);
+   glVertex2i(27,225);
+   glVertex2i(500,225);
+   glVertex2i(425,250);
+   glVertex2i(425,200);
+   glVertex2i(425,250);
+   glVertex2i(375,250);
+   glVertex2i(375,250);
+   glVertex2i(375,200);
+   glVertex2i(375,200);
+   glVertex2i(425,200);
+   
   glEnd();
+  
+    
   glFlush();
+  
 
 }
-
-
 int main(int argc, char** argv){
   glutInit(&argc,argv);
   glutInitDisplayMode(GLUT_SINGLE|GLUT_RGB);
-  glutInitWindowSize(640,840);
-  glutInitWindowSize(100,150);
+  glutInitWindowSize(640,480);
+  glutInitWindowPosition(100,150);
   glutCreateWindow("My first program");
   glutDisplayFunc(display);
   myInit();
