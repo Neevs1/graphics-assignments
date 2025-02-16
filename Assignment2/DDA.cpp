a35de91bc2ced62e01a225a3894942da2f28fb68
Neevs1 <neevan.redkar@gmail.com>
2025-02-16 17:51:14 +0530

    Added all types of line, axes and a boat

--- Assignment2/DDA.cpp
+++ Assignment2/DDA.cpp
@@ -55,12 +55,106 @@ float y = y1;
 sign(DX);
 sign(DY);
 glBegin(GL_POINTS);
-int counter = 0;
+
 for (int i = 0; i <= length; i++) {
     
        glColor3f(0.0, 0.0, 0.0);    
        glVertex2i(round(x), round(y));
       
+
+  
+x +=(DX);
+y +=(DY);
+}
+glEnd();
+glFlush();
+}
+
+void dotted_ddaLine(int x1, int y1, int x2, int y2) {
+  if(x1==x2&&y1==y2){
+  glBegin(GL_POINTS);
+  glVertex2i(x1, y1);
+  glEnd();
+  glFlush();
+  cout<<"Error - Origin and Destination are same"<<endl;
+  return;
+
+}
+int dx = x2 - x1;
+int dy = y2 - y1;
+int length;
+if(abs(dx)>abs(dy)){
+length=dx;
+}
+else{
+length=dy;
+}
+float DX = dx / float(length);
+float DY = dy / float(length);
+float x = x1;
+float y = y1;
+
+sign(DX);
+sign(DY);
+glBegin(GL_POINTS);
+
+for (int i = 0; i <= length; i++) {
+     if(int(x)%2==0&&int(y)%2==0){
+     glColor3f(0.0, 0.0, 0.0);    
+     glVertex2i(round(x), round(y));
+     }else{
+      glColor3f(1.0, 1.0, 1.0);
+      glVertex2i(round(x), round(y));
+     }
+
+
+x +=(DX);
+y +=(DY);
+}
+glEnd();
+glFlush();
+}
+
+void dashed_ddaLine(int x1, int y1, int x2, int y2) {
+  if(x1==x2&&y1==y2){
+    glBegin(GL_POINTS);
+    glVertex2i(x1, y1);
+    glEnd();
+    glFlush();
+    cout<<"Error - Origin and Destination are same"<<endl;
+    return;
+
+}
+int dx = x2 - x1;
+int dy = y2 - y1;
+int length;
+if(abs(dx)>abs(dy)){
+length=dx;
+}
+else{
+length=dy;
+}
+float DX = dx / float(length);
+float DY = dy / float(length);
+float x = x1;
+float y = y1;
+
+sign(DX);
+sign(DY);
+glBegin(GL_POINTS);
+int counter = 0;
+for (int i = 0; i <= length; i++) {
+       if(counter<10){
+       glColor3f(0.0, 0.0, 0.0);    
+       glVertex2i(round(x), round(y));
+       counter++;
+       }else if(counter>=10&&counter<20){
+        glColor3f(1.0, 1.0, 1.0);
+        glVertex2i(round(x), round(y));
+        counter++;
+       }else{
+        counter=0;
+       }
 
   
 x +=(DX);
@@ -76,7 +170,17 @@ void plotAxis(){
 }
 void display() {
 glClear(GL_COLOR_BUFFER_BIT);
-ddaLine(200, 150, 320, 150);
+ddaLine(100, 75,200, 150);
+dotted_ddaLine(-50, -50, -20, -50);
+dashed_ddaLine(-200, 100, -50, 100);
+ddaLine(25, -150, 275, -150);//boat starts here
+ddaLine(50, -200, 250, -200);
+ddaLine(50, -200, 25, -150);
+ddaLine(250, -200, 275, -150);
+ddaLine(150, -150, 150, -50);
+ddaLine(75, -112, 225, -112);
+ddaLine(75, -112, 150, -50);
+ddaLine(150, -50, 225, -112);
 plotAxis();
 glutSwapBuffers();
 }
