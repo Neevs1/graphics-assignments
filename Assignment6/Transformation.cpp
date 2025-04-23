940e951c9e103ece548eb7c5e7e20e700aaa4224
Neevs1 <neevan.redkar@gmail.com>
2025-04-23 10:29:57 +0530

    Added code for transformations with shear

--- Assignment6/Transformation.cpp
+++ Assignment6/Transformation.cpp
@@ -7,136 +7,240 @@
  *
 */
 
-#include <GL/glut.h>;
-#include <stdio.h>;
-#include <cmath>;
-static int flag;
-int length, xi, yi, choice;
-double angle, ET[3][3], ETResult[3][3];
-double Rh[4][4], RhResult[4][4];
+#include <GL/glut.h>
+#include <iostream>
+#include <cmath>
+using namespace std;
 
-}
+float vertices[100][2];
+int   numVertices = 0;
 
-DRAW
-
-void drawET(double ET[3][3]){
-
-int i;
-
-glBegin(GL_LINE_LOOP);
-for(i=0;i<3;i++)
-
-glVertex2i(ET[i][0],ET[i][1]);
-}
-glEnd();
-
-void drawR(double Rh[4][4]){
-
-int i;
-
-glBegin(GL_LINE_LOOP);
-for(i=0;i<4;i++){
-
-glVertex2i(Rh[i][0],Rh[i][1]);
-}
-glEnd();
-}
-
-void Display(){
-
-glClearColor(0,0,0,0);
-glClear(GL_COLOR_BUFFER_BIT);
-
-glLoadIdentity();
-gluOrtho2D(-320,320,-240,240);//note
-
-glColor3f(1,1,1);
-glBegin(GL_LINES);
-glVertex2d(-320,0);
-glVertex2d(320,0);
-glVertex2d(0,-240);
-glVertex2d(0,240);
-glEnd();
-
-glColor3f(1,0,0);
-if(flag == 0)
-drawET(ET);
-else if(flag == 1)
-drawR(Rh);
-glFlush();
-}
-
-void mult3X3(double ET[3][3],double temp[3][3])
+void init()
 {
-double sum;
-int i,j,k;
-for(i=0;i&lt;3;i++)
-{
-for(j=0;j&lt;3;j++)
-{
-sum=0;
-for(k=0;k&lt;3;k++)
-{
-sum=sum+ET[i][k]*temp[k][j];
+    glClearColor(1,1,1,1);
+    glPointSize(4);
+    glMatrixMode(GL_PROJECTION);
+    glLoadIdentity();
+    gluOrtho2D(-320,320,-240,240);
 }
-ETResult[i][j]=sum;
-}
-}
-}
-void mult4X4(double Rh[4][4],double temp[4][4])
+float scaleX = 1.0f, scaleY = 1.0f;
+float rotateAngle = 0.0f;
+float shearX  = 0.0f;          
+float shearY  = 0.0f;          
+bool  reflectX = false;
+bool  reflectY = false;
+float origTranslateX = -300.0f, origTranslateY = 0.0f;
+void multiplyMatrix(float A[3][3], float B[3][3], float R[3][3])
 {
-double sum;
-int i,j,k;
-for(i=0;i&lt;4;i++)
-{
-for(j=0;j&lt;4;j++)
-{
-sum=0;
-for(k=0;k&lt;4;k++)
-{
-sum=sum+Rh[i][k]*temp[k][j];
-}
-RhResult[i][j]=sum;
-}
-}
+    for (int i = 0; i < 3; ++i)
+        for (int j = 0; j < 3; ++j)
+        {
+            R[i][j] = 0;
+            for (int k = 0; k < 3; ++k)
+                R[i][j] += A[i][k] * B[k][j];
+        }
 }
 
-// Translation //
-void translationET()
+void applyTransformation(float M[3][3], float& x, float& y)
 {
-double tx,ty,temp[3][3];
-printf(&quot;\nTranslating Equilateral triangle&quot;);
-printf(&quot;\nEnter Tx: &quot;);
-scanf(&quot;%lf&quot;,&amp;tx);
-printf(&quot;\nEnter Ty: &quot;);
-scanf(&quot;%lf&quot;,&amp;ty);
-temp[3][3]={0};
-temp[0][0]=1;
-temp[1][1]=1;
-temp[2][2]=1;
-temp[2][0]=tx;
-temp[2][1]=ty;
-mult3X3(ET,temp);
-glColor3f(0.0,1.0,0.0);
-drawET(ETResult);
+    float p[3] = { x, y, 1.0f }, r[3] = {0};
+    for (int i = 0; i < 3; ++i)
+        for (int j = 0; j < 3; ++j)
+            r[i] += M[i][j] * p[j];
+    x = r[0];  y = r[1];
 }
 
-void translationRh()
+
+void getTransformationMatrix(float F[3][3])
 {
-double tx,ty,temp[4][4];
-printf(&quot;\nTranslating Rhombus&quot;);
-printf(&quot;\nEnter Tx: &quot;);
-scanf(&quot;%lf&quot;,&amp;tx);
-printf(&quot;\nEnter Ty: &quot;);
-scanf(&quot;%lf&quot;,&amp;ty);
-temp[4][4]={0};
-temp[0][0]=1;
-temp[1][1]=1;
-temp[2][2]=1;
-temp[3][3]=1;
-temp[3][0]=tx;
-temp[3][1]=ty;
-mult4X4(Rh,temp);
-glColor3f(0.0,1.0,0.0);
-drawR(RhResult);
+    
+    float S[3][3] = {
+        { scaleX, 0, 0 },
+        { 0, scaleY, 0 },
+        { 0, 0, 1 }
+    };
+
+
+    float H[3][3] = {
+        { 1, shearX, 0 },
+        { shearY, 1, 0 },
+        { 0, 0, 1 }
+    };
+
+    
+    float rad = rotateAngle * 3.14159f / 180.0f;
+    float R[3][3] = {
+        { cos(rad), -sin(rad), 0 },
+        { sin(rad),  cos(rad), 0 },
+        {    0,         0,    1 }
+    };
+
+    /* reflect */
+    float Ref[3][3] = {
+        { reflectY ? -1.0f : 1.0f, 0, 0 },
+        { 0, reflectX ? -1.0f : 1.0f, 0 },
+        { 0, 0, 1 }
+    };
+
+    
+    float T1[3][3], T2[3][3], T3[3][3];
+    multiplyMatrix(H,  S,   T1);   // H·S
+    multiplyMatrix(R,  T1,  T2);   // R·H·S
+    multiplyMatrix(Ref,T2,  T3);   // Ref·R·H·S
+
+    
+    for (int i = 0; i < 3; ++i)
+        for (int j = 0; j < 3; ++j)
+            F[i][j] = T3[i][j];
+}
+
+
+void ddaLine(int x1, int y1, int x2, int y2)
+{
+    int dx = x2 - x1, dy = y2 - y1;
+    int steps = max(abs(dx), abs(dy));
+    float Xinc = dx / float(steps);
+    float Yinc = dy / float(steps);
+    float x = x1, y = y1;
+
+    glBegin(GL_POINTS);
+    for (int i = 0; i <= steps; ++i)
+    {
+        glVertex2i(round(x), round(y));
+        x += Xinc;
+        y += Yinc;
+    }
+    glEnd();
+}
+
+
+void drawAxes()
+{
+    glColor3f(0,0,0);
+    ddaLine(-320,0, 320,0);
+    ddaLine(0,-240, 0,240);
+}
+
+
+void computeCentroid(float& cx, float& cy)
+{
+    cx = cy = 0;
+    for (int i = 0; i < numVertices; ++i)
+    {
+        cx += vertices[i][0];
+        cy += vertices[i][1];
+    }
+    cx /= numVertices;
+    cy /= numVertices;
+}
+
+
+void display()   //Display Function
+{
+    glClear(GL_COLOR_BUFFER_BIT);
+    drawAxes();
+
+    /* draw original (yellow) */
+    glColor3f(1,1,0);
+    glBegin(GL_POLYGON);
+    for (int i = 0; i < numVertices; ++i)
+        glVertex2f(vertices[i][0] + origTranslateX,
+                   vertices[i][1] + origTranslateY);
+    glEnd();
+
+    /* composite matrix */
+    float M[3][3];
+    getTransformationMatrix(M);
+
+    /* centroid for origin‑centred transform */
+    float cx, cy;
+    computeCentroid(cx, cy);
+
+    /* draw transformed (green) */
+    glColor3f(0,0.8,0);
+    glBegin(GL_POLYGON);
+    for (int i = 0; i < numVertices; ++i)
+    {
+        float x = vertices[i][0] - cx;
+        float y = vertices[i][1] - cy;
+        applyTransformation(M, x, y);
+        glVertex2f(x, y);
+    }
+    glEnd();
+
+    glutSwapBuffers();
+    glFlush();
+}
+
+
+void menu(int option) //Menu Creation
+{
+    switch(option)
+    {
+        case 1:  // scale
+            cout << "scaleX scaleY: ";
+            cin >> scaleX >> scaleY;
+            break;
+        case 2:  // rotate
+            cout << "rotation angle (deg): ";
+            cin >> rotateAngle;
+            break;
+        case 3:  // reflect Y
+            reflectY = !reflectY; break;
+        case 4:  // reflect X
+            reflectX = !reflectX; break;
+        case 5:  // shear
+            cout << "shearX shearY: ";
+            cin >> shearX >> shearY;
+            break;
+        case 6:  // reset
+            scaleX = scaleY = 1.0f;
+            rotateAngle = 0.0f;
+            shearX = shearY = 0.0f;
+            reflectX = reflectY = false;
+            break;
+    }
+    glutPostRedisplay();
+}
+
+void createMenu()
+{
+    int sub = glutCreateMenu(menu);
+    glutAddMenuEntry("Scale",1);
+    glutAddMenuEntry("Rotate",2);
+    glutAddMenuEntry("Reflect Y-axis",3);
+    glutAddMenuEntry("Reflect X-axis",4);
+    glutAddMenuEntry("Shear",5);
+    glutAddMenuEntry("Reset Transformations",6);
+
+    glutCreateMenu(menu);
+    glutAddSubMenu("Transformations", sub);
+    glutAttachMenu(GLUT_RIGHT_BUTTON);
+}
+
+
+void inputVertices()
+{
+    cout << "Number of vertices: ";
+    cin  >> numVertices;
+    for (int i = 0; i < numVertices; ++i)
+    {
+        cout << "Vertex " << i+1 << " (x y): ";
+        cin  >> vertices[i][0] >> vertices[i][1];
+    }
+}
+
+int main(int argc, char** argv)
+{
+    inputVertices();
+
+    glutInit(&argc, argv);
+    glutInitDisplayMode(GLUT_SINGLE | GLUT_RGB);
+    glutInitWindowSize(640,480);
+    glutCreateWindow("2D Transformations with Shear");
+    init();
+    createMenu();
+    glutDisplayFunc(display);
+    glutMainLoop();
+    return 0;
 }
